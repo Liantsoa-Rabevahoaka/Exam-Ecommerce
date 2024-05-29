f3d33ce94d82b563710c7fea1967e41c9616c5eb
Liantsoa <liantsoa.rabevahoaka@gmail.com>
2024-05-30 02:05:11 +0300

    add: description box

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati debitis incidunt quasi laboriosam, ut explicabo quia cumque consequatur? Alias amet neque exercitationem enim officia porro modi sit ullam maiores quam.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati debitis incidunt quasi laboriosam, ut explicabo quia cumque consequatur? Alias amet neque exercitationem enim officia porro modi sit ullam maiores quam.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
