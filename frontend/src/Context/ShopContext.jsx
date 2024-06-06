c7c16bb0c7c0b9fd71bcf4670ab09e92e30a3751
Liantsoa <liantsoa.rabevahoaka@gmail.com>
2024-06-07 01:04:30 +0300

    add counter to count the number of item in our cart

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -35,8 +35,18 @@ const ShopContextProvider = (props) =>{
         }
         return totalAmount;
     }
+
+    const getTotalCartItems = () =>{
+        let totalItem = 0;
+        for (const item in cartItems){
+            if (cartItems[item]>0){
+                totalItem+=cartItems[item];
+            }
+        }
+        return totalItem;
+    }
     
-    const contextValue = {getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart}; //stock the data
+    const contextValue = {getTotalCartItems,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart}; //stock the data
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
