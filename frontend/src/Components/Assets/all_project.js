57c4485629bd622c206912b8a3419b406d546d30
Liantsoa <liantsoa.rabevahoaka@gmail.com>
2024-06-18 02:44:47 +0300

    Natacha: add frontend page to the payment

---
+++ frontend/src/Components/Assets/all_project.js
@@ -0,0 +1,312 @@
+import p1_img from "../Assets/p16.jpg"
+import p2_img from "../Assets/product_16.jpg"
+import p3_img from "../Assets/p14.jpg"
+import p4_img from "../Assets/product_13.jpg"
+import p5_img from "../Assets/product_10.jpg"
+import p6_img from "../Assets/product_17.jpg"
+import p7_img from "../Assets/p20.jpg"
+import p8_img from "../Assets/p13.jpg"
+import p11_img from "../Assets/k_9.jpg"
+import p12_img from "../Assets/k_13.jpg"
+import p13_img from "../Assets/k_12.jpg"
+import p14_img from "../Assets/k_15.jpg"
+import p1 from "../Assets/p1.jpg"
+import p2 from "../Assets/p2.jpg"
+import p3 from "../Assets/p3.jpg"
+import p4 from "../Assets/p4.jpg"
+import p5 from "../Assets/p5.jpg"
+import p6 from "../Assets/p12.jpg"
+import p7 from "../Assets/p7.jpg"
+import p8 from "../Assets/p8.jpg"
+import product_1 from "../Assets/product_1.jpg"
+import product_2 from "../Assets/product_2.jpg"
+import product_3 from "../Assets/product_3.jpg"
+import product_4 from "../Assets/product_4.jpg"
+import product_5 from "../Assets/product_5.jpg"
+import product_13 from "../Assets/product-4.jpg"
+import product_14 from "../Assets/product_14.jpg"
+import p21 from "../Assets/p21.jpg"
+import k_1 from "../Assets/k_1.jpg"
+import k_2 from "../Assets/k_2.jpg"
+import k_3 from "../Assets/k_3.jpg"
+import k_4 from "../Assets/k_4.jpg"
+import k_5 from "../Assets/k_5.jpg"
+import k_6 from "../Assets/k_6.jpg"
+import k_7 from "../Assets/k_7.jpg"
+import k_8 from "../Assets/k_8.jpg"
+
+let all_product = [
+    {
+        id: 1,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "men",
+        image: p1_img,
+        new_price: 74,
+        old_price: 120
+    },
+    {
+        id: 2,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "men",
+        image: p3_img,
+        new_price: 74,
+        old_price: 120
+    },
+    {
+        id: 3,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "men",
+        image: p8_img,
+        new_price: 74,
+        old_price: 120
+    },
+    {
+        id: 4,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "men",
+        image: p7_img,
+        new_price: 74,
+        old_price: 120
+    },
+    {
+        id: 5,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "men",
+        image: p1,
+        new_price: 74,
+        old_price: 120
+    },{
+        id: 6,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "men",
+        image: p2,
+        new_price: 74,
+        old_price: 120
+    },{
+        id: 7,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "men",
+        image: p3,
+        new_price: 74,
+        old_price: 120
+    },{
+        id: 8,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "men",
+        image: p4,
+        new_price: 74,
+        old_price: 120
+    },{
+        id: 9,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "men",
+        image: p5,
+        new_price: 74,
+        old_price: 120
+    },{
+        id: 10,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "men",
+        image: p6,
+        new_price: 74,
+        old_price: 120
+    },{
+        id: 11,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "men",
+        image: p7,
+        new_price: 74,
+        old_price: 120
+    },{
+        id: 12,
+        name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+        category: "men",
+        image: p8,
+        new_price: 74,
+        old_price: 120
+    },
+    {
+        id: 13,
+        name: "Striped Flutter Sleeve Overlop Collar  Peplium Hern Blouse",
+        category: "women",
+        image: p2_img,
+        new_price: 60,
+        old_price: 120
+    },
+    
+    {
+        id: 14,
+        name: "Striped Flutter Sleeve Overlop Collar  Peplium Hern Blouse",
+        category: "women",
+        image: p4_img,
+        new_price: 60,
+        old_price: 120
+    },
+    {
+        id: 15,
+        name: "Striped Flutter Sleeve Overlop Collar  Peplium Hern Blouse",
+        category: "women",
+        image: p5_img,
+        new_price: 60,
+        old_price: 120
+    },
+    {
+        id: 16,
+        name: "Striped Flutter Sleeve Overlop Collar  Peplium Hern Blouse",
+        category: "women",
+        image: p6_img,
+        new_price: 60,
+        old_price: 120
+    },
+    {
+        id: 17,
+        name: "Striped Flutter Sleeve Overlop Collar  Peplium Hern Blouse",
+        category: "women",
+        image: product_1,
+        new_price: 60,
+        old_price: 120
+    },{
+        id: 18,
+        name: "Striped Flutter Sleeve Overlop Collar  Peplium Hern Blouse",
+        category: "women",
+        image: product_2,
+        new_price: 60,
+        old_price: 120
+    },{
+        id: 19,
+        name: "Striped Flutter Sleeve Overlop Collar  Peplium Hern Blouse",
+        category: "women",
+        image: product_3,
+        new_price: 60,
+        old_price: 120
+    },{
+        id: 20,
+        name: "Striped Flutter Sleeve Overlop Collar  Peplium Hern Blouse",
+        category: "women",
+        image: product_4,
+        new_price: 60,
+        old_price: 120
+    },{
+        id: 21,
+        name: "Striped Flutter Sleeve Overlop Collar  Peplium Hern Blouse",
+        category: "women",
+        image: product_5,
+        new_price: 60,
+        old_price: 120
+    },
+    {
+        id: 22,
+        name: "Striped Flutter Sleeve Overlop Collar  Peplium Hern Blouse",
+        category: "women",
+        image: product_13,
+        new_price: 60,
+        old_price: 120
+    },
+    {
+        id: 23,
+        name: "Striped Flutter Sleeve Overlop Collar  Peplium Hern Blouse",
+        category: "women",
+        image: p21,
+        new_price: 60,
+        old_price: 120
+    },
+    {
+        id: 24,
+        name: "Striped Flutter Sleeve Overlop Collar  Peplium Hern Blouse",
+        category: "women",
+        image: product_14,
+        new_price: 60,
+        old_price: 120
+    },
+    {
+        id: 25,
+        name: "Orange Colourblocked Hooded Sweotshirt",
+        category: "kid",
+        image: p11_img,
+        new_price: 74,
+        old_price: 120
+    },
+    {
+        id: 26,
+        name: "Orange Colourblocked Hooded Sweotshirt",
+        category: "kid",
+        image: p12_img,
+        new_price: 74,
+        old_price: 120
+    },
+    {
+        id: 27,
+        name: "Orange Colourblocked Hooded Sweotshirt",
+        category: "kid",
+        image: p13_img,
+        new_price: 74,
+        old_price: 120
+    },
+    {
+        id: 28,
+        name: "Orange Colourblocked Hooded Sweotshirt",
+        category: "kid",
+        image: p14_img,
+        new_price: 74,
+        old_price: 120
+    },
+    {
+        id: 29,
+        name: "Orange Colourblocked Hooded Sweotshirt",
+        category: "kid",
+        image: k_1,
+        new_price: 74,
+        old_price: 120
+    },{
+        id: 30,
+        name: "Orange Colourblocked Hooded Sweotshirt",
+        category: "kid",
+        image: k_2,
+        new_price: 74,
+        old_price: 120
+    },{
+        id: 31,
+        name: "Orange Colourblocked Hooded Sweotshirt",
+        category: "kid",
+        image: k_3,
+        new_price: 74,
+        old_price: 120
+    },{
+        id: 32,
+        name: "Orange Colourblocked Hooded Sweotshirt",
+        category: "kid",
+        image: k_4,
+        new_price: 74,
+        old_price: 120
+    },{
+        id: 33,
+        name: "Orange Colourblocked Hooded Sweotshirt",
+        category: "kid",
+        image: k_5,
+        new_price: 74,
+        old_price: 120
+    },{
+        id: 34,
+        name: "Orange Colourblocked Hooded Sweotshirt",
+        category: "kid",
+        image: k_6,
+        new_price: 74,
+        old_price: 120
+    },{
+        id: 35,
+        name: "Orange Colourblocked Hooded Sweotshirt",
+        category: "kid",
+        image: k_7,
+        new_price: 74,
+        old_price: 120
+    },{
+        id: 36,
+        name: "Orange Colourblocked Hooded Sweotshirt",
+        category: "kid",
+        image: k_8,
+        new_price: 74,
+        old_price: 120
+    }
+]
+
+export default all_product;
